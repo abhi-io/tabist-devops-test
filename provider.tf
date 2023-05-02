92e58aa32702dcff7cce6bdba8662e4d50e2f70b
abhi <abhi>
2023-05-02 14:05:52 +0530

    add: s3 backend

--- provider.tf
+++ provider.tf
@@ -3,7 +3,24 @@ provider "aws" {
   region = var.region
 }
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.1"
 }
 
 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         	   = "test-tf-devops-24324"
+    key              	 = "state/terraform.tfstate"
+    region         	   = "ap-south-1"
+    encrypt        	   = true
+    # dynamodb_table = "test_tf_devops_tf_lockid_98453"
+  }
+}
